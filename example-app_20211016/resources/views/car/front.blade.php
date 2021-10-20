d90e69547cddc22200f1e12f40ce50587453a25a
Thomas-Chiu <josemich0511@gmail.com>
2021-10-20 22:41:19 +0800

    routing practice

--- example-app_20211016/resources/views/car/front.blade.php
+++ example-app_20211016/resources/views/car/front.blade.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-    </head>
-    <body>
-        <h1>我是 /views/car/front.blade.php</h1>
-        <!-- 原生寫法 (不好) -->
-        <a href="./page1.blade.php">page1.blade.php</a>
-        <br />
-        <a href="./page2.blade.php">page2.blade.php</a>
-        <hr />
-        <!-- laravel blade 寫法 -->
-        <a href="{{ route('page1_name') }} ">laravel page1</a>
-        <br />
-        <a href="{{ route('page2_name') }} ">laravel page2</a>
-    </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>我是 /views/car/front.blade.php</h1>
+  <!-- 原生寫法 (無效) -->
+  <a href="./page1.blade.php">page1.blade.php</a>
+  <br />
+  <a href="./page2.blade.php">page2.blade.php</a>
+  <hr />
+  <!-- laravel blade 寫法 -->
+  <a href="{{ route('page1_name') }} ">laravel page1</a>
+  <br />
+  <a href="{{ route('page2_name') }} ">laravel page2</a>
+</body>
+
 </html>
--- example-app_20211016/routes/web.php
+++ example-app_20211016/routes/web.php
@@ -5,69 +5,59 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\SingleController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    $url = route("abc.dev_page1");
-    echo $url . "<br>";
-    echo $url . "<br>";
-    // dd() 可當斷點 debug 用
-    dd($url);
-    echo $url . "<br>";
-    echo $url . "<br>";
+  $url = route("abc.dev_page1");
+  echo $url . "<br>";
+  echo $url . "<br>";
+  // dd() 可當斷點 debug 用
+  dd($url);
+  echo $url . "<br>";
+  echo $url . "<br>";
 
-    // return view('welcome');
+  // return view('welcome');
 });
 
 // 練習 2: 導向 front 頁面後放兩個 <a> 連結 (page1、2)
 Route::get('/front', function () {
-    return view('car.front');
+  return view('car.front');
 })->name('front_name');
 
 Route::get('/page1', function () {
-    return view('car.page1');
+  return view('car.page1');
 })->name('page1_name');
 
 Route::get('/page2', function () {
-    return view('car.page2');
+  return view('car.page2');
 })->name('page2_name');
 
 // 練習 3: 利用網址 GET 參數回覆
 Route::get("/user/{id}/name/{name}", function ($id, $name) {
-    // '' 單引號放字串 "" 雙引號可放變數
-    return "<h1> Hello $id $name </h1>";
+  // '' 單引號放字串 "" 雙引號可放變數
+  return "<h1> Hello $id $name </h1>";
 });
 
-// 練習 4: Route Prefixes 路由字首
-/* Route::prefix("dev")->group(function () {
-    Route::get("/dev_page1", function () {
-        return view("dev.dev_page1");
-    });
+// 練習 4: Route Prefixes 路由前綴
+// Route::prefix("dev")->group(function () {
+//   Route::get("/dev_page1", function () {
+//     return view("dev.dev_page1");
+//   });
 
-    Route::get("/dev_page2", function () {
-        return view("dev.dev_page2");
-    });
-}); */
+//   Route::get("/dev_page2", function () {
+//     return view("dev.dev_page2");
+//   });
+// });
 
 // 練習 5: Route Name Prefixes 路由名稱字首
-// URL 命名是要給 route() 看的 
 Route::prefix("dev")->name("abc.")->group(function () {
-    Route::get("/dev_page1", function () {
-        return view("dev.dev_page1");
-    })->name("dev_page1");
+  Route::get("/dev_page1", function () {
+    // URL = /dev/dev_page1，路由名稱 = abc.dev_page1
+    return view("dev.dev_page1");
+  })->name("dev_page1");
 
-    Route::get("/dev_page2", function () {
-        return view("dev.dev_page2");
-    })->name("dev_page2");
+  Route::get("/dev_page2", function () {
+    // URL = /dev/dev_page2，路由名稱 = abc.dev_page2
+    return view("dev.dev_page2");
+  })->name("dev_page2");
 });
 
 
@@ -78,8 +68,8 @@ Route::get('/single2', [SingleController::class, "test"]);
 
 // 練習 1: 建立 controller CarController 
 // 再從 CarController 導向 views 畫面
-Route::resource('/cars', CarController::class);
-Route::resource('/photos', PhotoController::class);
+Route::resource('cars', CarController::class);
+Route::resource('photos', PhotoController::class);
 
 // 練習 7: 用 URL 方式導向 CarController 的 method
 // Resource Controllers, index, create, show, edit
